8553ceaff2d1a06611b3c5eef1500c4ca5353067
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2020-06-06 19:13:49 +0300

    packet printer implemented as thread

--- PacketHandler.py
+++ PacketHandler.py
@@ -7,12 +7,15 @@ import subprocess
 
 class PacketHandler(Thread):
     packet_queue = queue.Queue()
+    print_queue = queue.Queue()
 
     def __init__(self):
-       # Call the Thread class's init function
-       Thread.__init__(self)
-       self.rule_table = RuleTable.getInstance()
-       self.prompt_handler = PromptHandler()
+        # Call the Thread class's init function
+        Thread.__init__(self)
+        self.rule_table = RuleTable.getInstance()
+        self.prompt_handler = PromptHandler()
+        t = Thread(target=self.print_packet)
+        t.start()
 
     def run(self):
         while True:
@@ -20,6 +23,7 @@ class PacketHandler(Thread):
                 packet = PacketHandler.packet_queue.get()
                 #port = packet.sport
                 packet_payload = IP(packet.get_payload())
+                PacketHandler.print_queue.put(packet_payload)
                 ip = packet_payload.dst
                 rule = self.rule_table.get_rule_of_packet_ip(ip)
                 if rule is not None:
@@ -28,21 +32,29 @@ class PacketHandler(Thread):
                     else:
                         packet.drop()
                 else:
-                    self.print_packet(packet_payload)
-                    self.prompt_handler.add_packet_to_queue(packet)
                     packet.accept()
+                    self.prompt_handler.add_packet_to_queue(packet)
                 #host_name = get_host_name(ip)
             except KeyboardInterrupt:
                 break
-            except Exception:
+            except Exception as e:
+                print(e)
                 continue
     
-    def print_packet(self, packet):
-        print(packet.dst, packet.sport, self.get_application_name(packet.sport))
+    def print_packet(self):
+        while True:
+            try:
+                packet = PacketHandler.print_queue.get()
+                print(packet.dst, packet.sport, self.get_application_name(packet.sport))
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print(e)
+                continue
 
     def get_application_name(self, port):
         lsof = subprocess.Popen("sudo lsof -i :{}".format(port), shell=True, stdout=subprocess.PIPE).stdout
-        lsof_out_lines = lsof.split('\n')
+        lsof_out_lines = lsof.read().decode().split('\n')
         applications = []
         for i in range(1, len(lsof_out_lines)):
             application_name = lsof_out_lines[i].split(' ')[0]
